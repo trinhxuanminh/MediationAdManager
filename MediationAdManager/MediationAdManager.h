642a2853a2cff07b410ca77347af3f0ccb0b1392
trinhxuanminh2000 <trinhxuanminh2000@gmail.com>
2022-10-10 17:02:19 +0700

    Initial Commit

---
+++ MediationAdManager/MediationAdManager.h
@@ -0,0 +1,18 @@
+//
+//  MediationAdManager.h
+//  MediationAdManager
+//
+//  Created by Trịnh Xuân Minh on 10/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MediationAdManager.
+FOUNDATION_EXPORT double MediationAdManagerVersionNumber;
+
+//! Project version string for MediationAdManager.
+FOUNDATION_EXPORT const unsigned char MediationAdManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediationAdManager/PublicHeader.h>
+
+
